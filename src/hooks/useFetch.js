e3a4a9dbe219030ef480fc36569c51b048aa2af0
michael Kaner <michael.kaner@towa.at>
2020-10-24 20:23:51 +0200

    adde use effect to custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,23 +5,25 @@ export default function useFetch (location, setColorHash){
 
     const [status, setStatus] = React.useState('idle')
     const [weatherData, setWeatherData] = React.useState({})
-   
-    async function getWeather(){
-        if(location){
-            try{
-                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
-                const temp = response.data.main.temp;
-                setWeatherData(temp)
-                setColorHash(temp)
-                setStatus('success')
-            
-            }catch(e){
-                setStatus('error')  
+    
+    React.useEffect(()=>{
+        async function getWeather(){
+            if(location){
+                try{
+                    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
+                    const temp = response.data.main.temp;
+                    setWeatherData(temp)
+                    setColorHash(temp)
+                    setStatus('success')
+                
+                }catch(e){
+                    setStatus('error')  
+                }
             }
+           
         }
-       
-    }
-    getWeather()
-
+        getWeather()
+    }, [location, setColorHash])
+   
     return {status, weatherData}
 }
